1707dcd04385cd5175f03a4feeeff84b0c4e48e6
florez <florez@ethz.ch>
2025-12-05 13:42:55 +0100

    style(lifter): add lgpl header

--- tatva/lifter.py
+++ tatva/lifter.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2025 ETH Zurich (Mohit Pundir)
+#
+# This file is part of tatva.
+#
+# tatva is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tatva is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with tatva.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from __future__ import annotations
 
 from abc import ABC
